8dbf057c598e8de2c756196a615f866ef51b3606
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-01-21 17:19:45 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,1 @@
-import Notiflix from 'notiflix';
 
-export const fetchCountries = name => {
-  const requiredProperties = 'name,capital,population,flags,languages';
-  return fetch(
-    `https://restcountries.com/v2/name/${name}?fields=${requiredProperties}`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Oops, there is no country with that name');
-      }
-      return response.json();
-    })
-    .catch(error => {
-      Notiflix.Notify.failure(error.message);
-    });
-};
